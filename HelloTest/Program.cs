f37707c81a6b75faad22b20096681d4142b9345b
Ramsey Bland <ramsey.bland@gmail.com>
2018-09-24 20:42:38 -0600

    Made a randum num guesser

--- HelloTest/Program.cs
+++ HelloTest/Program.cs
@@ -10,12 +10,52 @@ namespace HelloTest
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            int RandNum = GenerateRandNum();
+            Boolean playing = true;
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.WriteLine("Pick a number between 1 and 10");
+
+            while(playing)
+            {
+                string usersGuess = Console.ReadLine();
+                
+                if(usersGuess == "end")
+                {
+                    playing = false;
+                } 
+                else
+                {
+                    int usersNum;
+                    int.TryParse(usersGuess, out usersNum);
+                    ValidateGuess(usersNum, RandNum);
+                }
+            }
+        }
+
+        private static int GenerateRandNum()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, 11);
+        }
+
+        private static void ValidateGuess(int usersNum, int RandNum)
+        {
+            if (usersNum == RandNum)
+            {
+                Console.WriteLine("Good Guess!!!");
+            }
+            else if (usersNum > 10 || usersNum < 1)
+            {
+                Console.WriteLine("invalid guess...");
+            }
+            else if (usersNum > RandNum)
+            {
+                Console.WriteLine("Too High!");
+            }
+            else if (usersNum < RandNum)
+            {
+                Console.WriteLine("Too Low!");
+            }
         }
     }
 }
